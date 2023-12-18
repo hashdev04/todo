dd83182cec312f665365967249e9892353c783bf
hashdev04 <hashimjj6t@gmail.com>
2023-12-19 00:39:43 +0500

    convert class based views to function based views

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ListListView.as_view(), name="index"),
-    path('list/<int:list_id>/', views.ItemListView.as_view(), name='list')
+    # path('', views.ListListView.as_view(), name="index"),
+    path('', views.list_todos, name="index"),
+    # path('list/<int:list_id>/', views.ItemListView.as_view(), name='list')
+    path('list/<int:list_id>/', views.get_todo_items, name="list"),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -5,18 +5,29 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-class ListListView(ListView):
-    model = TodoList
-    template_name = 'todo_app/index.html'
+# function based view
+def list_todos(request):
+    todos = TodoList.objects.all()
+    return render(request, 'todo_app/index.html', {'todos': todos})
 
-class ItemListView(ListView):
-    model = TodoListItem
-    template_name = 'todo_app/todo_list.html'
+# A class based view
+# class ListListView(ListView):
+#     model = TodoList
+#     template_name = 'todo_app/index.html'
 
-    def get_queryset(self):
-        return TodoListItem.objects.filter(todo_list_id=self.kwargs['list_id'])
+def get_todo_items(request, list_id):
+    todo = TodoList.objects.get(id=list_id)
+    items = TodoListItem.objects.filter(todo_list_id=list_id)
+    return render(request, 'todo_app/todo_list.html', {'todo': todo, 'items': items})
+
+# class ItemListView(ListView):
+#     model = TodoListItem
+#     template_name = 'todo_app/todo_list.html'
+
+#     def get_queryset(self):
+#         return TodoListItem.objects.filter(todo_list_id=self.kwargs['list_id'])
     
-    def get_context_data(self):
-        context = super().get_context_data()
-        context['todo_list'] = TodoList.objects.get(id=self.kwargs['list_id'])
-        return context
+#     def get_context_data(self):
+#         context = super().get_context_data()
+#         context['todo_list'] = TodoList.objects.get(id=self.kwargs['list_id'])
+#         return context
